a0181dd82ee7e5e0b74a2204f80f750e517ad173
atian155 <atianthony@outlook.com>
2024-03-09 15:37:52 +0200

    Implemented transaction conversion functions

---
+++ assets/scripts/main.js
@@ -0,0 +1,66 @@
+const API_URLS = {
+  fetch: "https://ivory-ostrich-yoke.cyclic.app/students/available",
+  convert: "https://ivory-ostrich-yoke.cyclic.app/students/convert",
+};
+
+const totalBalance = document.querySelectorAll(".total-balance");
+
+const getCurrencies = async () => {
+  if (localStorage.hasOwnProperty("currencies")) {
+    return;
+  }
+  const result = await fetch(API_URLS.fetch);
+  const data = await result.json();
+  // Save currencies to local storage
+  // to avoid repeatedly calling the API
+  localStorage.currencies = JSON.stringify(data);
+  return;
+};
+
+const convertCurrency = async (from = "USD", to = "USD", amount = 0.0) => {
+  if (from === to) {
+    return parseFloat(amount);
+  }
+  const result = await fetch(API_URLS.convert, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      from: from,
+      to: to,
+      amount: parseFloat(amount),
+    }),
+  });
+  const data = await result.json();
+  return data;
+};
+
+// https://www.freecodecamp.org/news/how-to-format-number-as-currency-in-javascript-one-line-of-code/
+const USDollar = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+const calculateBalance = () => {
+  const transactions = JSON.parse(localStorage.transactions ?? "[]");
+  let balance = 0;
+  transactions.map((trans) => {
+    const amount = trans.usdAmount;
+    const type = trans.type;
+    switch (type) {
+      case "income":
+        balance += amount;
+        break;
+      case "expense":
+        balance -= amount;
+        break;
+    }
+  });
+  const formatted = USDollar.format(balance);
+  totalBalance.forEach((el) => (el.textContent = `${formatted}`));
+  return formatted;
+};
+
+// Calculate balance on load
+calculateBalance();
--- assets/scripts/pages/transaction.js
+++ assets/scripts/pages/transaction.js
@@ -1,3 +1,77 @@
-let balance = 0;
-let balanceCurrency = "USD";
+const title = document.getElementById("transaction-title");
+// Form
+const transForm = document.getElementById("trans_form");
+const [currencySelector, transAmount] = [
+  document.getElementById("currency_selector"),
+  document.getElementById("trans_amount"),
+];
+let prevCurrency = "USD";
 
+// Check if edit mode
+const param = new URLSearchParams(window.location.search).get("edit");
+if (param) {
+  title.textContent = "Edit Transaction";
+}
+
+// Fetch currencies and populate
+// currency dropdown
+getCurrencies().then(() => {
+  const currencies = JSON.parse(localStorage.currencies);
+  currencies.map((currency) => {
+    const { name, symbol, code } = currency;
+    currencySelector.innerHTML += `<option value="${code}">${name} (${symbol})</option>`;
+  });
+});
+
+currencySelector.addEventListener("change", async (e) => {
+  // Disable amount field
+  toggleDisabled(transAmount, true);
+
+  const newCurrency = e.target.value;
+  // Convert currencies
+  const convert = await convertCurrency(
+    prevCurrency,
+    newCurrency,
+    parseFloat(transAmount.value)
+  );
+  const data = await convert;
+  transAmount.value = data;
+  toggleDisabled(transAmount, false);
+  prevCurrency = newCurrency;
+});
+
+transForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  createTransaction(
+    currencySelector.value,
+    transAmount.value,
+    document.querySelector('input[name="trans_type"]:checked').value
+  );
+});
+
+const createTransaction = async (
+  currency = "USD",
+  amount = 0.0,
+  type = "income"
+) => {
+  const transactions = JSON.parse(localStorage.transactions ?? "[]");
+  const transObject = {
+    id: (transactions[transactions.length - 1]?.id ?? 0) + 1,
+    currency: currency,
+    amount: amount,
+    usdAmount: await convertCurrency(currency, "USD", amount).then(
+      (data) => data
+    ),
+    type: type,
+  };
+  localStorage.transactions = JSON.stringify([...transactions, transObject]);
+  calculateBalance();
+};
+
+const toggleDisabled = (element, condition = true) => {
+  if (condition) {
+    element.setAttribute("disabled", true);
+    return;
+  }
+  element.removeAttribute("disabled");
+};
